657b41d8c6d2b2ce1336a78c7948aa609e663589
Bart Noordervliet <bart@noordervliet.net>
2017-11-06 21:39:08 +0100

    Make update script bump dbversion too

--- db/update-v1-to-v2.sql
+++ db/update-v1-to-v2.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "note" ADD COLUMN deleted bool default false;
 CREATE TABLE "link" (id integer primary key, source integer not null, target integer not null, name text, FOREIGN KEY(source) REFERENCES note(id), FOREIGN KEY(target) REFERENCES note(id));
 CREATE TABLE "auth_token" (id integer primary key, hash text not null, expires integer not null);
+UPDATE "setting" SET value = 2 WHERE name = 'dbversion';
