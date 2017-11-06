1817915bbc316e6e4a8bcb7283ecc13b6170c647
Bart Noordervliet <bart@noordervliet.net>
2017-11-06 20:19:51 +0100

    Add v1 to v2 SQL update script

---
+++ db/update-v1-to-v2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "note" ADD COLUMN deleted bool default false;
+CREATE TABLE "link" (id integer primary key, source integer not null, target integer not null, name text, FOREIGN KEY(source) REFERENCES note(id), FOREIGN KEY(target) REFERENCES note(id));
+CREATE TABLE "auth_token" (id integer primary key, hash text not null, expires integer not null);
